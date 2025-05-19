8843a4c5c954d0c8f257c978316d993b241b95ed
JoseSLK <jose.salamanca05@uptc.edu.co>
2025-05-19 18:06:30 -0500

    Implemented all components in education content

--- src/components/Education_content/index.jsx
+++ src/components/Education_content/index.jsx
@@ -1,14 +1,71 @@
-import React from "react";
+import React, { useEffect, useState  } from "react";
 import "./educationContent.css";
+import { useContent } from "../../Context/ContentContext";
+import { ArticleItem } from "./ArticleItem";
+import { Article } from "./Article";
 
 export function EducationContent () {
+    const { content, searchContent, searchArticles } = useContent();
+    const [searchQuery, setSearchQuery] = useState("");
+    const [debouncedTerm, setDebouncedTerm] = useState("");
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleSearch = async (e) => {
+        const term = e.target.value || "";
+        setSearchQuery(term);
+    }
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    useEffect(() => {
+        if(!searchQuery.trim()){
+            setDebouncedTerm("");
+            return;
+        }
+        if (searchQuery.trim().length > 3) {
+            const timer = setTimeout( () => {
+                setDebouncedTerm(searchQuery.trim());
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [searchQuery]);
+
+    useEffect(() => {
+        if (debouncedTerm) {
+            searchArticles(debouncedTerm);
+        }
+    }, [debouncedTerm, searchArticles]);
+
+    const articlesToRender = debouncedTerm && searchContent?.length > 0
+    ? searchContent
+    : content;
+
     return (
         <div className="dou-education-content">
-            <nav className="dou-menu-content">
-                <input type="text" className="dou-login-input search" />
-            </nav>
+            <button className="menu-toggle" onClick={toggleMenu}>
+                {isMenuOpen ? '✕' : '☰'}
+            </button>
+            <div className={`dou-menu-content ${isMenuOpen ? 'active' : ''}`}>
+                <input 
+                    type="text" 
+                    className="dou-login-input search" 
+                    placeholder="Buscar articulos..."
+                    value={searchQuery}
+                    onChange={handleSearch}
+                />
+
+                {articlesToRender?.length > 0 ? (
+                    articlesToRender.map(article => (
+                        <ArticleItem article={article} key={article.material_id} />
+                    ))
+                ) : (
+                    <p>No se encontraron artículos.</p>
+                )}
+            </div>
             <div className="dou-content">
-                Algo
+                <Article />
             </div>
         </div>
     );
