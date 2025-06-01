5f6162f03f8302c540886e9b032fcdb2c986a8c6
JoseSLK <jose.salamanca05@uptc.edu.co>
2025-05-31 22:52:37 -0500

    Add id in route when select an exercise

--- src/components/Education_content/ArticleItem.jsx
+++ src/components/Education_content/ArticleItem.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { useContent } from "../../Context/ContentContext";
+import { Link, useNavigate } from "react-router-dom";
 
 export function ArticleItem ( { article, isSelected } ) {
     const { material_id, title } = article;
     const { setSelectedMaterial } = useContent();
+    const navigate = useNavigate();
 
     const handleClick = () => {
-        setSelectedMaterial( article );
+        setSelectedMaterial(article);
+        navigate(`/dashboard/education/${material_id}`);
     };
 
     const tupleClassName = `dou-tuple ${isSelected? 'dou-tuple-selected' : ''}`;
     return (
         <div className={tupleClassName} onClick={handleClick}>
-            <span className="dou-tuple-id">ID: {material_id}</span>
-            <span className="dou-tuple-name">{title}</span>
+            <Link to={`/dashboard/education/${material_id}`}>
+                <span className="dou-tuple-id">ID: {material_id}</span>
+                <span className="dou-tuple-name">{title}</span>
+            </Link>
         </div>
     )
 }
